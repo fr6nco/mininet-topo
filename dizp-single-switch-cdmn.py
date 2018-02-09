e8cdf485475a23e7fb0bed71554ea27a6aa63eea
Thomas Boros <thomas@thomass-mbp.guest.tno.nl>
2018-02-09 13:40:10 +0100

    removed rrtest for now, disabled xterms as it is for some reason not working and starting web server on rr

--- dizp-single-switch-cdmn.py
+++ dizp-single-switch-cdmn.py
@@ -4,8 +4,8 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import CPULimitedHost, OVSSwitch, RemoteController
 from mininet.link import TCLink, Intf
-from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel, info
+from mininet.util import dumpNodeConnections, waitListening
+from mininet.log import setLogLevel, info, output, warn
 from mininet.cli import CLI
 from functools import partial
 
@@ -20,9 +20,9 @@ class SingleSwitchTopo(Topo):
 
 		mgsw = self.addSwitch('s66766') #DPID used for the Management switch
 		rr = self.addHost('rr', ip='10.10.0.4') #testing node, primarily testing on host
-		rrtest = self.addHost('rrtest', ip='10.10.0.5') #testing node, within the same mgt switch
+		# rrtest = self.addHost('rrtest', ip='10.10.0.5') #testing node, within the same mgt switch
 		self.addLink(rr, mgsw)
-		self.addLink(rrtest, mgsw)
+		# self.addLink(rrtest, mgsw)
 
 
 
@@ -30,13 +30,18 @@ def setup():
 	"Start Network"
 	topo = SingleSwitchTopo()
 	OVSSwitch13 = partial(OVSSwitch, protocols='OpenFlow13')
-	net = Mininet(topo=topo, ipBase='10.10.0.0/24', switch=OVSSwitch13, controller=RemoteController('c0', ip='192.168.56.1'), autoSetMacs=True, xterms=True)
+	net = Mininet(topo=topo, ipBase='10.10.0.0/24', switch=OVSSwitch13, controller=RemoteController('c0', ip='192.168.56.1'), autoSetMacs=True, xterms=False)
 
 	for h in net.hosts:
-		info('Disabling IPV6 for ' + str(h))
+		info('Disabling IPV6 for ' + str(h) + '\n')
 		h.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
 		h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
 		h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+		if str(h) == 'rr':
+			h.cmd('python -m SimpleHTTPServer 80 &')
+			info("Request Router is running a webserver now, You can connect at http://" + h.IP() + "/\n")
+
 
 	# for sw in net.switches:
 	# 	if sw.name == 's66766':
