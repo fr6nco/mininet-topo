5704b7838e487c8338bcbf6e70858220bcc4d031
Tomas Boros <tomas.boros@stuba.sk>
2019-04-18 11:15:31 +0200

    added linear build topo

---
+++ dizp_topo_rr_intnet.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+
+## This file brings up a networking and connects the RR and DNS as internal network, 
+## which runs at the services machine in our virtualbox
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost, OVSSwitch, RemoteController
+from mininet.link import TCLink, Intf
+from mininet.util import dumpNodeConnections, waitListening
+from mininet.log import setLogLevel, info, output, warn
+from mininet.cli import CLI
+from functools import partial
+
+class SingleSwitchTopo(Topo):
+	"Single switch topology"
+	def build(self):
+		client = self.addHost('client', ip='10.11.0.1') 
+		server = self.addHost('server', ip='10.11.0.2')
+		s1 = self.addSwitch('s1')
+		s2 = self.addSwitch('s2')
+
+		self.addLink(client, s1)
+		self.addLink(server, s2)
+		self.addLink(dns, s1)
+		self.addLink(s1, s2)
+
+		mgsw = self.addSwitch('s66766') # DPID used for the Management switch
+
+		self.addLink(mgsw, s2) # connect mgsw to core switch
+		self.addLink(mgsw, s1) # connect mgsw to core switch
+
+def setup():
+	"Start Network"
+	topo = SingleSwitchTopo()
+	OVSSwitch13 = partial(OVSSwitch, protocols='OpenFlow13')
+	net = Mininet(topo=topo, ipBase='10.11.0.0/24', switch=OVSSwitch13, controller=RemoteController('c0', ip='192.168.56.1'), autoSetMacs=True, xterms=False)
+
+	for h in net.hosts:
+		info('Disabling IPV6 for ' + str(h) + '\n')
+		h.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+		h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+		h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+		h.cmd("echo ''")
+
+		if str(h) in ['server',]:
+			h.cmd('python -m SimpleHTTPServer 80 &')
+			info("Request Router is running a webserver now, You can connect at http://" + h.IP() + "/\n")
+
+	for sw in net.switches:
+		if sw.name == 's66766':
+			_intf = Intf('enp0s9', node=sw)
+
+	net.start()
+	CLI(net)
+	net.stop()
+
+if __name__ == '__main__':
+	setLogLevel('info')
+	setup()
+
+
---
+++ linear_topo_prod.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+
+### Linear topology where clients are on the one end and service engines are on the other end.
+
+import sys
+import math
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost, OVSSwitch, RemoteController
+from mininet.link import TCLink, Intf
+from mininet.util import dumpNodeConnections, waitListening, irange
+from mininet.log import setLogLevel, info, output, warn
+from mininet.cli import CLI
+from functools import partial
+
+class CustomLinearTopo(Topo):
+
+	def __init__(switchcount):
+		self.switchcount = switchcount
+		super(CustomLinearTopo, self).__init__()
+
+	"""
+	Linear topology
+	"""
+	def build(self):
+
+		# build switches
+        switches = [ self.addSwitch( 's%s' % s )
+                     for s in irange( 1, self.switchcount - 1 ) ]
+
+        # Wire up switches
+        last = None
+        for switch in switches:
+            if last:
+                self.addLink( last, switch )
+            last = switch
+
+def setup():
+	"Start Network"
+	topo = CustomLinearTopo()
+	OVSSwitch13 = partial(OVSSwitch, protocols='OpenFlow13')
+	net = Mininet(topo=topo, ipBase='10.11.0.0/24', switch=OVSSwitch13, controller=RemoteController('c0', ip='10.9.1.11'), autoSetMacs=True, xterms=False)
+
+	for sw in net.switches:
+		## Clients
+		if sw.name == 's1':
+			_intf = Intf('eth5', node=sw)
+			_intf = Intf('eth6', node=sw)
+		
+		## Surrogates
+		if sw.name == 's%s' % topo.switchcount - 1:
+			_intf = Intf('eth3', node=sw)
+			_intf = Intf('eth4', node=sw)
+
+		
+		if sw.name == 's%s' % str(math.floor(topo.switchcount)):
+			_intf = Intf('eth1', node=sw)
+			_intf = Intf('eth2', node=sw)
+
+	net.start()
+	CLI(net)
+	net.stop()
+
+if __name__ == '__main__':
+	# setLogLevel('info')
+
+	if len(sys.argv) < 2:
+		print "Integer as arguemnt required"
+		exit(1)
+
+	try:
+		print "Starting Topo with " + str(int(sys.argv[1])) + " switches"
+	except ValueError:
+		print "argument must be integer"
+
+	# setup()
+
+
